acddfec47058d79a677cd853bbfdd028178a5255
abcdeSky <84276259+abcdeSky@users.noreply.github.com>
2021-05-18 01:00:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,8 @@ from discord.ext import commands, tasks
 import os
 import traceback
 import random
+from datetime import datetime, timedelta
+import sched
 import time
 
 description = '''An example bot to showcase the discord.ext.commands extension
@@ -10,6 +12,9 @@ module.
 There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='?', description=description)
 token = os.environ['DISCORD_BOT_TOKEN']
+
+def def1(name):
+    print name
 
 @bot.event
 async def on_ready():
@@ -29,8 +34,20 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def repeat(ctx, times : int, interval : int, content='repeating...'):
+async def repeat(ctx, times : int, hour : int, min : int, content='repeating...'):
     """Repeats a message multiple times."""
+    now = datetime.now()
+    now = datetime(now.year, now.month, now.day, now.hour, now.minute, 0)
+    comp = datetime(now.year, now.month, now.day, now.hour + hour, now.minute + min, 0)
+    diff = comp - now
+
+    if int(diff.days) >= 0:
+        scheduler = sched.scheduler(time.time, time.sleep)
+        scheduler.enter(diff.seconds, 1, def1, ("hoge", ))
+        scheduler.run()
+    else:
+        print "do nothing"
+
     for i in range(times):
         await ctx.send(content)
         time.sleep(interval)
@@ -42,12 +59,8 @@ async def slow_count():
 @slow_count.after_loop
 async def after_slow_count():
     print('done!')
-    
-@bot.command()
-async def count(ctx):
-    slow_count.start()
 
-#slow_count.start()
+slow_count.start()
 
 
 bot.run(token)
