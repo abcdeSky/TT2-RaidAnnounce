be7dbb7fdb27281b625b314d315460e00a38f45b
abcdeSky <84276259+abcdeSky@users.noreply.github.com>
2021-05-18 00:03:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,11 +2,20 @@ import discord
 from discord.ext import commands
 import os
 import traceback
+import random
 
-
-bot = commands.Bot(command_prefix='/')
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+bot = commands.Bot(command_prefix='?', description=description)
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 @bot.event
 async def on_command_error(ctx, error):
